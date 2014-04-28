03fabb7a6e8ddfc3d987fe4ef6e773a8e291440f
Art Lim <artjplim@gmail.com>
2014-04-28 08:42:49 +0800

    First Commit

---
+++ src/sad/SAD.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sad;
+
+/**
+ *
+ * @author ajbuster
+ */
+public class SAD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
